0fb441932c5a57b58748fc6fb0bf3b6e2e845e72
brahim bahri <ibra.bahri@gmail.com>
2015-10-25 22:33:22 +0100

    irqa second activity

---
+++ app/src/main/java/com/quran/psm/iqra/MainActivity.java
@@ -0,0 +1,67 @@
+package com.quran.psm.iqra;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import com.quran.psm.iqra.NouvelleKhatma;
+
+public class MainActivity extends Activity {
+
+    private Button creer;
+    private Button enCours;
+    private Button douaa;
+    private Button quitter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        quitter=(Button)this.findViewById(R.id.quitter_button);
+        quitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+
+            }
+        });
+
+        creer=(Button)findViewById(R.id.nouvelle_khatma_button);
+        creer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, NouvelleKhatma.class));
+            }
+        });
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/quran/psm/iqra/NouvelleKhatma.java
@@ -0,0 +1,17 @@
+package com.quran.psm.iqra;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by brahim on 10/25/15.
+ */
+public class NouvelleKhatma extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nouvelle_khatma);
+
+
+    }
+}
